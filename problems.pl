e54e347c46c3000ddfc9bd241aba34b2fbd27f8e
Shawn M Moore <sartak@gmail.com>
2011-11-05 12:32:23 -0400

    Complain about missing reviews of read books

--- problems.pl
+++ problems.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/env perl
 use 5.14.0;
+use utf8::all;
 use warnings;
 use Text::CSV_XS;
 
@@ -8,8 +9,10 @@ die "Cannot use CSV: ".Text::CSV_XS->error_diag ();
 open my $fh, "<:encoding(utf8)", "goodreads_export.csv" or die "goodreads_export.csv: $!";
 scalar <$fh>;
 while (my $row = $csv->getline ($fh)) {
-    warn $row->[1]
+    warn "Missing japanese or english shelf: $row->[1]\n"
         unless $row->[16] =~ /english/ || $row->[16] =~ /japanese/;
+    warn "Missing review of read book: $row->[1]\n"
+        if $row->[16] !~ /currently-reading|wont-read|to-read/ && $row->[7] eq '0';
 }
 $csv->eof or $csv->error_diag ();
 close $fh;
